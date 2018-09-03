428678a2b10f981139b90f6459233dfe26995c80
onep2p <324834500@qq.com>
2018-09-03 15:06:37 +0800

    update

---
+++ app/order/controller/AdminIndexController.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.chouvc.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 逸秋 < 324834500@qq.com>
+// +----------------------------------------------------------------------
+namespace app\order\controller;
+
+use cmf\controller\AdminBaseController;
+
+class AdminIndexController extends AdminBaseController
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+}
