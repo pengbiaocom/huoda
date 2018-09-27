bdd820f03cca97895f73d983b6993ff1359578a2
pengbiaocom <pengbiaocom56@163.com>
2018-09-27 23:16:31 +0800

    1111

--- api/wxapp/controller/AddressController.php
+++ api/wxapp/controller/AddressController.php
@@ -38,4 +38,52 @@ class AddressController extends Controller{
         return json($district);
     }
 
+    public function  get_address_byid(Request $request){
+        $id = $request->param('id');
+
+        if(empty($id))  return json(['code'=>1,'msg'=>'缺少参数']);
+
+        $info = db("user_address")->where(['id'=>$id])->find();
+        if($info){
+            return json(['code'=>0,'msg'=>'success','data'=>$info]);
+        }else{
+            return json(['code'=>1,'msg'=>'没有数据']);
+        }
+    }
+
+    public function get_address(Request $request){
+        $keyword = $request->param('keyword');
+        $uid = $request->param('uid');
+
+        if(empty($keyword)) return json(['code'=>1,'msg'=>'缺少参数']);
+//        $where = " "
+    }
+
+    public function  add(Request $request){
+
+        $data['province_id'] = $request->param('province_id');
+        $data['city_id'] = $request->param('city_id');
+        $data['county_id'] = $request->param('county_id');
+        $data['address'] = $request->param('address');
+        $data['user_name'] = $request->param('user_name');
+        $data['user_tel'] = $request->param('user_tel');
+        $data['uid'] = $request->param('uid');
+        $data['update_time'] = time();
+        $data['types'] = $request->param('types');
+
+        if(empty($data['user_name'])){
+            return json(['code'=>1,'msg'=>'请输入发件人姓名']);
+        }else if(!preg_match("/^1[345678]{1}\d{9}$/",$data['user_tel'])){
+            return json(['code'=>1,'msg'=>'请输入正确的手机号']);
+        }else if(empty($data['province_id']) || empty($data['city_id']) || empty($data['county_id']) || empty($data['address'])){
+            return json(['code'=>1,'msg'=>'请填写地址']);
+        }else{
+            if($id = db("user_address")->insertGetId($data)){
+                return json(['code'=>0,'msg'=>'新增地址成功','id'=>$id]);
+            }else{
+                return json(['code'=>1,'msg'=>'新增地址失败']);
+            }
+        }
+    }
+
 }
