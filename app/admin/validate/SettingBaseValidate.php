ddb2338cd495ef3d3be75e69fb52ad7526393463
逸秋 <324834500@qq.com>
2018-10-27 21:31:22 +0800

    update

--- app/admin/validate/SettingBaseValidate.php
+++ app/admin/validate/SettingBaseValidate.php
@@ -16,7 +16,6 @@ class SettingBaseValidate extends Validate
 {
     protected $rule = [
         'options.dispatch_max_num' => 'require',
-        'options.big_max_num' => 'require',
         'options.notice' => 'require',
 //         'options.about' => 'require',
 //         'options.user_agreement' => 'require',
@@ -25,7 +24,6 @@ class SettingBaseValidate extends Validate
 
     protected $message = [
         'options.dispatch_max_num.require' => '出车最大送单量不能为空',
-        'options.big_max_num.require' => '出车最大运送大件量不能为空',
         'options.notice.require' => '公告不能为空',
 //         'options.about.require' => '关于我们不能为空',
 //         'options.user_agreement.require' => '用户协议不能为空',
