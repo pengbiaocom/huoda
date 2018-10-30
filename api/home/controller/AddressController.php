0025febd6ab2d5387e05fcfaefdaf2fd13914630
onep2p <324834500@qq.com>
2018-10-30 17:15:12 +0800

    update
    新增地址接口（分单个和所有的）
    
    所有只支持支付或以上状态的订单

---
+++ api/home/controller/AddressController.php
@@ -0,0 +1,47 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.chouvc.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 逸秋 < 324834500@qq.com>
+// +----------------------------------------------------------------------
+namespace api\home\controller;
+
+use cmf\controller\RestBaseController;
+use think\Db;
+
+class AddressController extends RestBaseController
+{
+    public function read()
+    {
+        $type = $this->request->param('type', 0, 'intval');// 0  最近的单个地址，1  最近的所有地址
+		$uid = $this->request->param("uid", 0, 'intval');
+
+		switch ($type) {
+		    case 0:
+		        $where['uid'] = $uid;
+		        $data = Db::name("order")->where($where)->order('create_time desc')->find();
+		    break;
+		    case 1:
+		        $where['order_status'] = array('GT', 0);
+		        if($uid > 0) $where['uid'] = $uid;
+		        $data = Db::name("order")->where($where)->order('create_time desc')->select();
+		    break;
+		    default:
+		        $where['uid'] = $uid;
+		        $data = Db::name("order")->where($where)->order('create_time desc')->find();
+		    break;
+		}
+		
+		if (empty($this->apiVersion) || $this->apiVersion == '1.0.0') {
+		    $response = [$data];
+		} else {
+		    $response = $data;
+		}
+		
+		$this->success("地址获取成功!", $response);
+    }
+}
--- api/home/route.php
+++ api/home/route.php
@@ -5,3 +5,4 @@ use think\Route;
 Route::resource('home/slides', 'home/Slides');
 Route::resource('home/cargo', 'home/Cargo');
 Route::resource('home/region', 'home/Region');
+Route::resource('home/address', 'home/Address');
